ac7dfe7e2107c488125e4844771b57abcb4a6f98
Sumit Harijan <65243909+smthari@users.noreply.github.com>
2020-12-31 23:26:47 +0530

    Add files via upload

---
+++ Documentation Page/index.js
@@ -0,0 +1,27 @@
+function myFunction0() {
+    var copyText = document.getElementById("select-this0");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
+
+function myFunction() {
+    var copyText = document.getElementById("select-this2");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
+
+function myFunction4() {
+    var copyText = document.getElementById("select-this4");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
+
+function myFunction5() {
+    var copyText = document.getElementById("select-this5");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
